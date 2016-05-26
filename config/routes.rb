d19cbe7eeb2239641f7b45ff39aa543d3fb7312e
kawashinshin <kawakamis@kawakamishintarou-no-MacBook-Pro.local>
2016-05-26 15:43:27 +0900

    ログイン機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
        root  "top#index"
        resources :top
+       resources :users, only: [:show]
 
 
   #   resources :products do
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160526031427) do
+ActiveRecord::Schema.define(version: 20160526063840) do
+
+  create_table "groups", force: :cascade do |t|
+    t.string   "key",        limit: 255
+    t.text     "detail",     limit: 65535
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -30,6 +37,11 @@ ActiveRecord::Schema.define(version: 20160526031427) do
     t.string   "unconfirmed_email",      limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.integer  "group_id",               limit: 4
+    t.string   "family_name",            limit: 255
+    t.string   "first_name",             limit: 255
+    t.string   "family_name_kana",       limit: 255
+    t.string   "first_name_kana",        limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
