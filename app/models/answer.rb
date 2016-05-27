f071d3ebd4bdd7010d6cdd83c82c5e3682d8aeff
kawashinshin <kawakamis@kawakamishintarou-no-MacBook-Pro.local>
2016-05-27 20:24:10 +0900

    コメントをタイムラインに記載する。

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,6 +4,7 @@ class Answer < ActiveRecord::Base
     has_one :feed_content, as: :content, dependent: :destroy
       #callback
   after_create :create_feed_content
+  after_update :update_feed_content
 
   #validation
   validates_presence_of :user_id, :text
@@ -12,4 +13,9 @@ class Answer < ActiveRecord::Base
   def create_feed_content
     self.feed_content = FeedContent.create(group_id: question.group_id, updated_at: updated_at)
   end
+  
+  def update_feed_content
+    self.feed_content.update(updated_at: updated_at)
+  end
+
 end
