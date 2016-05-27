4d8217e35d6f8bcc1ec0a3ee59bbe21b4695f1e5
kawashinshin <kawakamis@kawakamishintarou-no-MacBook-Pro.local>
2016-05-27 15:25:54 +0900

    マイページに自分が答えた回答を表示。　多対多のアソシエーション

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,13 @@
 class Question < ActiveRecord::Base
-  belongs_to :group
+  #association
   belongs_to :user
-  has_many :answers
+  belongs_to :group
+  has_many :answers, dependent: :destroy
 
+  #validation
   validates_presence_of :user_id, :text, :group_id
+
+  def user_answer(user_id)
+      Answer.find_by(user_id: user_id, question_id: id)
+  end
 end
